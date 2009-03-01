01687178f344dce3d1f5409419ecaf07e1a34931
Jack Danger Canty <gitcommit@6brand.com>
2009-02-28 18:31:56 -0800

    triggering flickr grab on document load

--- photos.js
+++ photos.js
@@ -3,23 +3,25 @@ var API_KEY = '3a26f36ac015fa09111911faeaf6eedc'
 
 // Original code from http://www.mattryall.net/blog/2008/06/flickr-api-and-jquery
 
-$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key="+API_KEY+"&photoset_id=72157600007733788&format=json&jsoncallback=?",
-  function (data) {
-    $.each(data.photoset.photo, function (i, photo) {
-        var image = $("<img/>")
-                    .attr("src",
-                        "http://farm" + photo.farm +
-                        ".static.flickr.com/" + photo.server +
-                        "/" + photo.id +
-                        "_" + photo.secret + "_s.jpg")
+$(function(){
+  $.getJSON("http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key="+API_KEY+"&photoset_id=72157600007733788&format=json&jsoncallback=?",
+    function (data) {
+      $.each(data.photoset.photo, function (i, photo) {
+          var image = $("<img/>")
+                      .attr("src",
+                          "http://farm" + photo.farm +
+                          ".static.flickr.com/" + photo.server +
+                          "/" + photo.id +
+                          "_" + photo.secret + "_s.jpg")
 
-        var link = $("<a/>").attr("title", photo.title)
-            .attr("href", "http://www.flickr.com/photos/smita_pednekar/" + photo.id)
-            .append(image)
+          var link = $("<a/>").attr("title", photo.title)
+              .attr("href", "http://www.flickr.com/photos/smita_pednekar/" + photo.id)
+              .append(image)
 
-        $("#photos").append(
-          $("<div/>").append(link)
-        );
-    });
-  }
-);
+          $("#photos").append(
+            $("<div/>").append(link)
+          );
+      });
+    }
+  );
+})
